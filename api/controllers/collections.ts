da5b1f00b3460fe020f63be470785b6cf1f27afa
calistar16 <60900402+calistar16@users.noreply.github.com>
2021-02-22 21:09:03 -0700

    Inital commit

---
+++ api/controllers/collections.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+
+export default function () {
+  return {
+    async getCollection (req: express.Request, res: express.Response) {
+      res.status(200)
+      res.send({'Collection': 123})
+    },
+    async createCollection (req: express.Request, res: express.Response) {
+      res.status(201)
+      res.send()
+    },
+    async getOneCollection (req: express.Request, res: express.Response) {
+      res.status(200)
+      res.send()
+    },
+    async renameCollection (req: express.Request, res: express.Response) {
+      const rename = "Collection renamed to '" + req.body.name + "'"
+      res.status(200)
+      res.send({'Message': rename})
+    }
+  }
+}
